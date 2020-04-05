f64d74112eb3704365fb8135bd2c1c292c48d1c8
MrPengueeen <aonmoy0.0@gmail.com>
2020-04-06 01:33:55 +0600

    Earthquake Data visualization using data file from RSS Feed

--- src/markTopFiveEarthquakes.java
+++ src/markTopFiveEarthquakes.java
@@ -21,27 +21,29 @@ public class markTopFiveEarthquakes extends PApplet {
 	
 	private UnfoldingMap map;
 	
+	//Top 5 earthquakes based on magnitude
+			//Create the Location objects for the 5 locations
+			
+			Location valEq = new Location(-39.825390, -73.231522);
+			Location alaskaEq = new Location(61.127151, -149.957214);
+			Location sumatraEq = new Location(-0.589724, 101.343109);
+			Location japanEq = new Location(40.727876, 141.257882);
+			Location kamchatkaEq = new Location(56.210949, 159.346725);
+			
+			//Create point features
+			PointFeature valFeat = new PointFeature(valEq);
+			PointFeature alaskaFeat = new PointFeature(alaskaEq);
+			PointFeature sumatraFeat = new PointFeature(sumatraEq);
+			PointFeature japanFeat = new PointFeature(japanEq);
+			PointFeature kamchatkaFeat = new PointFeature(kamchatkaEq);
+	
 	public void setup() {
 		size(900, 900, OPENGL);
 		map = new UnfoldingMap(this,  new Microsoft.HybridProvider());
 		map.zoomToLevel(2);
 		MapUtils.createDefaultEventDispatcher(this,  map);
 		
-		//Top 5 earthquakes based on magnitude
-		//Create the Location objects for the 5 locations
 		
-		Location valEq = new Location(-39.825390, -73.231522);
-		Location alaskaEq = new Location(61.127151, -149.957214);
-		Location sumatraEq = new Location(-0.589724, 101.343109);
-		Location japanEq = new Location(40.727876, 141.257882);
-		Location kamchatkaEq = new Location(56.210949, 159.346725);
-		
-		//Create point features
-		PointFeature valFeat = new PointFeature(valEq);
-		PointFeature alaskaFeat = new PointFeature(alaskaEq);
-		PointFeature sumatraFeat = new PointFeature(sumatraEq);
-		PointFeature japanFeat = new PointFeature(japanEq);
-		PointFeature kamchatkaFeat = new PointFeature(kamchatkaEq);
 		
 		addFeature(valFeat, valEq, "Valvidia, Chile", "9.5", "May 22, 1960", 1960);
 		addFeature(alaskaFeat, alaskaEq, "Alaska, US", "9.2", "March 27, 1964", 1964);
